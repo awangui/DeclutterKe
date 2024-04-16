12bc06e2b68d99c9e772fac9685a674c3a079569
wangui <86366610+awangui@users.noreply.github.com>
2024-04-16 23:53:25 +0300

    update card page

--- card.php
+++ card.php
@@ -1,53 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Product Page</title>
-  <link rel="stylesheet" href="styles.css">
-  <link rel="stylesheet" href="card.css">
-  <script src="https://kit.fontawesome.com/661ba5765b.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-  <script src="script.js"></script>
-  <style>
-    .thumbnail {
-      cursor: pointer;
-    }
-
-  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Property Details</title>
+    <script src="https://kit.fontawesome.com/661ba5765b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="draft.css">
 </head>
-
 <body>
-        <section class="sticky-nav">
+    <section class="sticky-nav">
         <button class="menu" onclick="menuToggle()"><i class="fa fa-bars"></i></button>
-<nav>
-    <a href="index.php" class="logo">
-        <img src="./images/Logo maker project (1).png" class="icon">
-        <b><span>Declutter</span> Ke</b>
-    </a>
-    <a href="#home">Home</a>
-    <a href="store.php" class="active">Store</a>
-    <a href="about.html">About</a>
-    <a href="#contact">Contact</a>
-    <a href="listing.php" class="cta">Add a Listing</a>
-    <div class="credentials">
-    <a href="login.html"><i class="icon fa-solid fa-right-to-bracket "></i> Login</a>
-    <a href="registration.html"><i class="icon fa-regular fa-user"></i> Sign Up</a>
-    </div>
+        <nav>
+            <a href="index.php" class="logo">
+                <img src="./images/Logo maker project (1).png" class="icon">
+                <b><span>Declutter</span> Ke</b>
+            </a>
+            <a href="index.php">Home</a>
+            <a href="store.php" class="active">Store</a>
+            <a href="about.html">About</a>
+            <a href="#contact">Contact</a>
+            <a href="listing.php" class="cta">Add a Listing</a>
+            <div class="credentials">
+                <a href="login.html"><i class="icon fa-solid fa-right-to-bracket "></i> Login</a>
+                <a href="registration.html"><i class="icon fa-regular fa-user"></i> Sign Up</a>
+            </div>
+        </nav>
+    </section>
 
-</nav>
-        </section>
-  <main>
-    <section id="product-images">
-      <?php
+    <div class="container">
+        <div class="property-images">
+        <?php
       include 'connection.php';
-      //check if user is logged in
-      // if(!isset($_SESSION['userId'])){
-      //     //redirect to login page
-      //     header("Location: login.html");
-      //     exit();
-      // }
       // Check if the listing_id is provided in the URL
       if (isset($_GET['listing_id'])) {
         $listing_id = $_GET['listing_id'];
@@ -60,66 +44,87 @@
         // Display product images
         if ($row) {
           $photosArray = explode(',', $row['photos']); // Split photos field by comma
-          // Display the first image as larger image
-          echo '<img id="mainImage" src="uploads/' . $photosArray[0] . '" alt="Product Image" class="card-img-top" style="width: 50%;">';
-          // Display the rest of the images underneath
-          echo '<div class="image-group">';
+          echo '<div class="image-gallery">';
+          echo '<img src="uploads/' . $photosArray[0] . '" alt="Product Image" id="mainImage">';
+          
+          echo '<div class="gallery-row">';
           for ($i = 1; $i < count($photosArray); $i++) {
-            echo '<img src="uploads/' . $photosArray[$i] . '" alt="Product Image" class="thumbnail" onclick="swapImage(this)">';
+            echo ' <img src="uploads/' . $photosArray[$i] . '" alt="Product Image" onclick="swapImage(this)">';
           }
           echo '</div>';
+          echo '</div>';
+         
         } else {
           echo "Product not found.";
         }
       } else {
         echo "Listing ID not provided.";
       }
-      ?>
-    </section>
-
-    <section id="product-details">
-      <?php
+      ?>            <div class="safety-tips">
+      <h2>Safety tips</h2>
+      <ul>
+          <li><b>1.Bring a Friend:</b> It's always a good idea to bring a friend or family member along when viewing a listing.</li>
+          <li><b>2.Inspect Items Thoroughly:</b> Before finalizing a purchase, inspect the item thoroughly for any damages or discrepancies. Ask for additional photos or information if needed.</li>
+          <li><b>3.Do not pay before viewing: </b> inspect the item in person before making any payments to ensure it matches its description and meets your expectations.</li>
+          <a href="listing.php" class="contact"><button class="post-like">Post Ad Like This</button></a>
+      </ul>
+    
+  </div>
+        </div>
+        <div class="property-details">
+        <?php
       // Check if the listing_id is provided in the URL
       if (isset($_GET['listing_id'])) {
         // Fetch product details based on the listing_id
         $query = "SELECT * FROM listings WHERE listing_id = $listing_id";
         $result = mysqli_query($con, $query);
         $row = mysqli_fetch_assoc($result);
-
-        // Display product details
         if ($row) {
-             // Retrieve phone number from the database
-             $phone_number = $row['phone_number'];
-      ?>
-          <div class="card">
-            <div class="card-body">
-              <h2 class="card-title"><?php echo $row['name']; ?></h2>
-              <p class="card-text"><?php echo $row['description']; ?></p>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Category: <span> <?php echo $row['category']; ?></li>
-                <li class="list-group-item">Sub Category: <span> <?php echo $row['sub_category']; ?></li>
-                <li class="list-group-item">Brand: <span> <?php echo $row['brand']; ?></li>
-                <li class="list-group-item">Color: <span> <?php echo $row['color']; ?></li>
-                <li class="list-group-item">Years Used: <span> <?php echo $row['years_used']; ?></li>
-                <li class="list-group-item">Condition: <span> <?php echo $row['condition']; ?></li>
-                <li class="list-group-item">Price: <span><?php echo 'ksh ' . $row['price']; ?></li>
-             
-              </ul>
-              <div class="actions">
-    <a href="#" class="wishlist">
-        <span class="tooltip-text">Add to wishlist!</span>
-        <a href="#" class="wishlist"><span class="material-symbols-outlined">
-favorite
-</span></a>
-    </a><br>
-    <?php
-    echo '<a href="https://wa.me/' . $phone_number . '" class="btn btn-primary">Contact Seller</a>';
-    ?>
-</div>
+          // Retrieve phone number from the database
+          $phone_number = $row['phone_number'];
+   ?>
+          
+            <h1 class="product-title"><?php echo $row['name'];  ?></h1>
+            <div class="listing-details">
+                <p class="posted-info">Posted:<span><?php echo $row['date_posted']; ?></span></li></p>
+                <p class="location">Town:<span><?php echo $row['city']; ?></p>
+                
+                <!-- Additional property details -->
+                <div class="property-info">
+                <li class="list-group-item">Category: <span> <?php echo $row['category']; ?></span></li>
+                <li class="list-group-item">Sub Category: <span> <?php echo $row['sub_category']; ?></span></li>
+                <li class="list-group-item">Brand: <span> <?php echo $row['brand']; ?></span></li>
+                <li class="list-group-item">Color: <span> <?php echo $row['color']; ?></span></li>
+                <li class="list-group-item">Years Used: <span> <?php echo $row['years_used']; ?></span></li>
+                <li class="list-group-item">Condition: <span> <?php echo $row['condition']; ?></span></li>
+            <p class="card-text"><?php echo $row['description']; ?></p>
+                </div>
 
+                <!-- Price and contact information -->
+                <div class="price-contact">
+                <li class="list-group-item">Price: <span><?php echo 'ksh ' . $row['price']; ?></span></li>
+                    <!-- Add contact options as needed -->
+                </div>
             </div>
-          </div>
-      <?php
+            <!-- Seller information -->
+            <div class="seller">
+                <h2>Seller details</h2>
+                <div class="seller-info">
+                    <p class="seller-name">Joseph Maina</p>
+                    <p class="seller-status">Posted by Ad ID: 1234</p>
+                    <p class="date-joined">Member since: 2021</p>
+                </div>
+                <!-- Add contact options as needed -->
+                <a href="https://wa.me/<?php echo $phone_number; ?>"  class="whatsapp">
+    <button class="btn">
+        <i class="fa-brands fa-whatsapp"></i> Chat on WhatsApp
+    </button>
+</a>
+
+                <a href="store.php" class="contact"><button class="btn"><i class="fa-solid fa-phone"></i>Show Contact</button></a>
+                <i class="fa-regular fa-heart"></i>
+            </div>
+            <?php
         } else {
           echo "Product not found.";
         }
@@ -127,10 +132,10 @@ favorite
         echo "Listing ID not provided.";
       }
       ?>
-    </section>
-
-  </main>
-  <section id="related-products">
+        </div>
+        </div>
+ 
+    <section id="related-products">
   <h2>
     <?php
     if (isset($row) && $row) {
@@ -161,7 +166,7 @@ favorite
       $sub_category = $row['sub_category'];
 
       // Fetch related products with similar category or subcategory, limited to 3
-      $related_query = "SELECT * FROM listings WHERE (category = '$category' OR sub_category = '$sub_category') AND listing_id != $listing_id LIMIT 4";
+      $related_query = "SELECT * FROM listings WHERE (category = '$category' OR sub_category = '$sub_category') AND listing_id != $listing_id ";
       $related_result = mysqli_query($con, $related_query);
 
       // Check if there are related products
@@ -218,8 +223,19 @@ favorite
   }
   ?>
 </section>
+        <!-- Safety tips -->
+        <div class="caution">
+       
+                <div class="disclaimer">
+          <h3>Disclaimer</h3>
+          <ul>
+<li>The platform serves as a marketplace for sellers to list their items, and while we strive to ensure the accuracy of the listings, we cannot guarantee the quality, safety, or legality of the items listed.</li>
+  <li> Buyers are advised to exercise caution, conduct thorough inspections, and verify the authenticity of items before making any purchases.</li>
+<li> Additionally, transactions conducted through this platform are solely between the buyer and the seller, and we are not responsible for any disputes or issues that may arise.</li>
+</ul>
+        </div>
+</div>
 
- 
     <section id="footer">
         <div class="footer-main">
             <div class="contain">
@@ -230,7 +246,6 @@ favorite
                         <li><a href="#">Benefits of reselling</a></li>
                         <li><a href="about.html">Our purpose</a></li>
                         <li><a href="#">Our buying process</a></li>
-                        
                     </ul>
                 </div>
                 <div class="contained">
@@ -240,7 +255,6 @@ favorite
                         <li><a href="#"><i class="fa fa-instagram"></i> Instagram</a></li>
                         <li><a href="#"><i class="fa fa-phone"> </i> +254 000 000 000</i></a></li>
                         <li><a href="#"><i class="fa-regular fa-envelope"></i></i> declutterke@gmail.com</a></li>
-                        <li></li>
 
                     </ul>
                 </div>
@@ -250,16 +264,11 @@ favorite
             <input type="email" name="email" placeholder="Your E-mail">
             <button type="submit">Subscribe</button>
         </form>
-        <!-- <span> &copy; Declutter Ke. All rights reserved.</span> -->
-        
     </div>
             </div>
-           
         </div>
-        
     </section>
-
-  <script>
+    <script>
     function swapImage(thumbnail) {
       var mainImage = document.getElementById('mainImage');
       mainImage.style.opacity = 0; // Fade out main image
@@ -272,5 +281,4 @@ favorite
     }
   </script>
 </body>
-
 </html>
