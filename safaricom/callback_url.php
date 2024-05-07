4a5db86cd737e61219241f925a8a6d12e42beda6
wangui <86366610+awangui@users.noreply.github.com>
2024-05-07 03:29:38 +0300

    saving to database

--- safaricom/callback_url.php
+++ safaricom/callback_url.php
@@ -12,23 +12,8 @@ require_once '../connection.php';
   }';
  
      // DATA
-    //  $mpesaResponse = file_get_contents('php://input');
-    $mpesaResponse = '{
-      "TransactionType": "Pay Bill",
-      "TransID":"RKTQDM7W6S",
-      "TransTime":"20191122063845",
-      "TransAmount":"10",
-      "BusinessShortCode": "600638",
-      "BillRefNumber":"invoice008",
-      "InvoiceNumber":"",
-      "OrgAccountBalance":"",
-      "ThirdPartyTransID": "",
-      "MSISDN":"25470****149",
-      "FirstName":"John",
-      "MiddleName":"",
-      "LastName":"Doe"
-    }';
-   
+     $mpesaResponse = file_get_contents('php://input');
+  
     $json = json_decode($mpesaResponse);    
     // Insert into mpesa_transaction table
     $query = "INSERT INTO mpesa_transactions (transaction_type,trans_id,trans_time,trans_amount,business_short_code,bill_ref_number,MSISDN,first_name,last_name) VALUES ('$json->TransactionType', '$json->TransID',  '$json->TransTime', '$json->TransAmount',  '$json->BusinessShortCode', '$json->BillRefNumber', '$json->MSISDN','$json->FirstName', '$json->LastName')";
