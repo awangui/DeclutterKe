8c3f47aa374bd6e6977a7c5ceb1a8c50243f24bb
wangui <86366610+awangui@users.noreply.github.com>
2024-05-09 23:33:21 +0300

    db update

--- database/resale_db.sql
+++ database/resale_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 06, 2024 at 06:33 PM
+-- Generation Time: May 09, 2024 at 10:31 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,13 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
+(1, 'Hisense'),
+(2, 'Mika'),
+(3, 'LG'),
+(4, 'Ramtons'),
+(5, 'otherbrand'),
+(6, 'Samsung'),
+(7, 'Hotpoint'),
+(8, 'Other');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
-  `CategoryID` int(15) NOT NULL,
-  `CategoryName` varchar(50) NOT NULL
+  `category_id` int(15) NOT NULL,
+  `category_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'furniture'),
+(2, 'Appliances'),
+(3, 'electronics'),
+(4, 'Kitchenware');
 
 -- --------------------------------------------------------
 
@@ -41,9 +76,7 @@ CREATE TABLE `categories` (
 CREATE TABLE `listings` (
   `listing_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `category` varchar(100) NOT NULL,
   `sub_category` varchar(100) NOT NULL,
-  `brand` varchar(100) NOT NULL,
   `color` varchar(50) NOT NULL,
   `years_used` int(11) NOT NULL,
   `condition` varchar(50) NOT NULL,
@@ -53,37 +86,29 @@ CREATE TABLE `listings` (
   `phone_number` varchar(15) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
   `town` varchar(100) DEFAULT NULL,
-  `date_posted` date DEFAULT NULL,
-  `seller_id` int(11) DEFAULT NULL
+  `date_posted` date DEFAULT curdate(),
+  `seller_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `brand_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `listings`
 --
 
-INSERT INTO `listings` (`listing_id`, `name`, `category`, `sub_category`, `brand`, `color`, `years_used`, `condition`, `price`, `description`, `photos`, `phone_number`, `city`, `town`, `date_posted`, `seller_id`) VALUES
-(12, 'chair', 'furniture', 'other', 'Hisense', 'Brown', 1, 'fairly used', 1200, 'dinner table chair', 'ag.jpg', NULL, 'Nyeri', NULL, '2024-05-01', 37),
-(13, 'tables', 'furniture', 'fridges', 'Mika', 'Black', 1, 'fairly used', 19000, 'a wooden dining table', 'ab67616d0000b273b5da98d1669cefce033629cb.jfif', NULL, 'Kiambu', NULL, '2024-05-01', 34),
-(14, 'tables', 'furniture', 'fridges', 'LG', 'Black', 4, 'fairly used', 23435, 'dfxbdf', 'ab67616d0000b273b5da98d1669cefce033629cb.jfif,ab67616d0000b2739c05fec02bd9b81ee1246b2f.jfif', NULL, 'Mombasa', NULL, '2024-05-01', 37),
-(15, 'epofpo', 'Appliances', 'fridges', 'Mika', 'Black', 3, 'new', 24545, 'vxddxv', '5e448c69ae6fa.image.jpg,1200x1200bb.jpg', NULL, 'Malindi', NULL, '2024-05-01', 31),
-(16, 'picnic bench', 'furniture', 'fridges', 'Ramtons', 'Bronze', 0, 'fairly used', 24555, 'cdfzzv', '_savage_ TikTok wallpaper.jpg,03fbc79adc620966730d1d464bc9db93.300x300x1.jpg,50-505075_spotify-playlist-covers-chill.jpg,100% THAT BXTCH.jpg', NULL, 'Kiambu', NULL, '2024-05-01', 35),
-(17, 'picnic bench', 'furniture', 'fridges', 'Ramtons', 'Bronze', 0, 'fairly used', 24555, 'cdfzzv', '_savage_ TikTok wallpaper.jpg,03fbc79adc620966730d1d464bc9db93.300x300x1.jpg,50-505075_spotify-playlist-covers-chill.jpg', NULL, 'Kisumu', NULL, '2024-05-01', 31),
-(22, 'Jbl Flip 5 Speaker', 'electronics', 'Speakers', 'otherbrand', 'Black', 1, 'fairly used', 15000, 'Portable jbl flip 5 speaker', 'download (9).jfif,speaker.jfif', '254711936444', 'Nairobi', 'Ruiru', '2024-05-01', 35),
-(24, 'LG Fridge', 'electronics', 'fridges', 'LG', 'Black', 1, 'fairly used', 20000, 'A fairly used LG fridge. Working well . Reason for sale is relocation', 'f2.jpg,f3.jpg,fridge.jpg', '254711936444', 'Nairobi', 'Ruiru', '2024-05-01', 35),
-(26, 'Desk', 'furniture', 'tables', 'otherbrand', 'Brown', 1, 'fairly used', 5000, 'quality desk', 'pexels-catherine-augustin-3049121.jpg', '254711936444', 'Nairobi', 'Ruiru', NULL, 37),
-(27, 'Bed Frame', 'furniture', 'other', 'otherbrand', 'Black', 3, 'used', 12000, 'Wooden bed frame', 'bed.jpeg,beds.jpeg', '254794879850', 'Muranga', 'Muranga town', NULL, 37),
-(28, 'Spotify', 'Kitchenware', 'Microwaves', 'Mika', 'Black', 2, 'new', 12000, 'a good microwave', '68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f79457a5f6b6554323268656171773d3d2d3630303238313433382e31353366356330363733356531653.jfif,ab67616d00001e02af37f5c01de02dc863bb7e9f.jpg,artworks-zS06HSfOPLbKBnhI-RqnNog-t500x500.jpg', '25411936444', 'Muranga', 'Chorong&#039;i', NULL, 31),
-(29, 'Microwave', 'Kitchenware', 'Microwaves', 'Samsung', 'Black', 2, 'fairly used', 7000, 'dsdiooins', 'm2.jpg,micro.jpg', '254711936444', 'Nairobi', 'Utawala', NULL, 35),
-(30, 'table', 'Appliances', 'Speakers', 'Hotpoint', 'other', 6, 'fairly used', 6789, 'buib', 'm2.jpg', '254711936444', 'Nairobi', 'iblip', '0000-00-00', NULL),
-(34, 'tables', 'furniture', 'phones', 'Ramtons', 'Black', 8, 'used', 99, 'jkion', 'pexels-alina-vilchenko-1173651.jpg', '254711936444', 'Nairobi', 'Ruiru', NULL, 35);
+INSERT INTO `listings` (`listing_id`, `name`, `sub_category`, `color`, `years_used`, `condition`, `price`, `description`, `photos`, `phone_number`, `city`, `town`, `date_posted`, `seller_id`, `category_id`, `brand_id`) VALUES
+(1, 'Bed Frame', 'bed', 'Black', 2, 'fairly used', 150000, 'A fully wooden bed frame', 'bed.jpeg,beds.jpeg', '254794879850', 'Nairobi', 'Ruiru', '2024-05-01', 35, 1, 8),
+(2, 'Microwave', 'Microwave', 'Black', 3, 'used', 6000, 'Black Samsung Microwave ', 'm2.jpg,micro.jpg', '254794879850', 'Nyeri', 'Chorong&#039;i', '2024-05-01', 35, 4, 6),
+(4, 'LG Fridge', 'fridge', 'black', 5, 'used', 40000, 'No frost fridge with built in ice dispenser', 'f2.jpg,f3.jpg,fridge.jpg', '254798417766', 'Nairobi', 'Karen', '2024-05-09', 35, 3, 3),
+(5, 'Coffee table', 'table', 'grey', 3, 'new', 4000, 'Wooden coffee table with two small top drawers', 'table.jfif', '254711936444', 'Kiambu', 'Thindigua', '2024-05-09', 35, 1, 8);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mpesa_transactions`
+-- Table structure for table `mpesa_request_details`
 --
 
-CREATE TABLE `mpesa_transactions` (
+CREATE TABLE `mpesa_request_details` (
   `transaction_type` varchar(255) NOT NULL,
   `trans_id` varchar(50) NOT NULL,
   `trans_time` datetime NOT NULL,
@@ -96,13 +121,43 @@ CREATE TABLE `mpesa_transactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `mpesa_transactions`
+-- Dumping data for table `mpesa_request_details`
 --
 
-INSERT INTO `mpesa_transactions` (`transaction_type`, `trans_id`, `trans_time`, `trans_amount`, `business_short_code`, `bill_ref_number`, `MSISDN`, `first_name`, `last_name`) VALUES
+INSERT INTO `mpesa_request_details` (`transaction_type`, `trans_id`, `trans_time`, `trans_amount`, `business_short_code`, `bill_ref_number`, `MSISDN`, `first_name`, `last_name`) VALUES
 ('Pay Bill', 'RKTQDM7W6S', '2019-11-22 06:38:45', 10, 600638, 'invoice008', '25470****149', 'John', 'Doe'),
 ('Pay Bill', 'RKTQDM7W6S', '2019-11-22 06:38:45', 10, 600638, 'invoice008', '25470****149', 'John', 'Doe'),
 ('Pay Bill', 'RKTQDM7W6S', '2019-11-22 06:38:45', 10, 600638, 'invoice008', '25470****149', 'John', 'Doe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mpesa_transactions`
+--
+
+CREATE TABLE `mpesa_transactions` (
+  `Amount` float NOT NULL,
+  `MpesaReceiptNumber` varchar(50) NOT NULL,
+  `TransactionDate` datetime NOT NULL,
+  `PhoneNumber` varchar(12) NOT NULL,
+  `MerchantRequestID` varchar(50) NOT NULL,
+  `CheckoutRequestID` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mpesa_transactions`
+--
+
+INSERT INTO `mpesa_transactions` (`Amount`, `MpesaReceiptNumber`, `TransactionDate`, `PhoneNumber`, `MerchantRequestID`, `CheckoutRequestID`) VALUES
+(0, 'cow', '0000-00-00 00:00:00', 'dog', 'rabbit', 'lamb'),
+(0, 'cow', '0000-00-00 00:00:00', 'dog', '-34591447', 'lamb'),
+(1, 'NLJ7RT61SV', '2019-12-19 10:21:15', '254708374149', '29115-34620561-1', 'ws_CO_191220191020363925'),
+(0, '', '0000-00-00 00:00:00', '', '', ''),
+(1, 'NLJ7RT61SV', '2019-12-19 10:21:15', '254708374149', '29115-34620561-1', 'ws_CO_191220191020363925'),
+(1, 'NLJ7RT61SV', '2019-12-19 10:21:15', '254708374149', '29115-34620561-1', 'ws_CO_191220191020363925'),
+(0, '', '0000-00-00 00:00:00', '', 'Request canceled by user.', ''),
+(0, 'Request canceled by user.', '0000-00-00 00:00:00', '1032', '29115-34620561-1', 'ws_CO_191220191020363925'),
+(0, '', '0000-00-00 00:00:00', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -122,6 +177,8 @@ CREATE TABLE `subscribers` (
 
 INSERT INTO `subscribers` (`email`, `subscriber_name`, `customer_id`) VALUES
 ('anitawangui22@gmail.com', 'Anita Wangui', 31),
+('bkk@gmail.com', 'Brian Kamau', 36),
+('rihomba72@yahoo.com', 'Rosemary Ihomba', 37),
 ('shakirasyevuo@gmail.com', 'Shakira Syevuo', 35);
 
 --
@@ -147,7 +204,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL DEFAULT 2,
-  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `date` date NOT NULL DEFAULT current_timestamp(),
   `category` enum('normal','seller','admin') NOT NULL DEFAULT 'normal'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -160,24 +217,34 @@ INSERT INTO `users` (`UserId`, `firstName`, `surname`, `email`, `password`, `rol
 (34, 'Francis', 'Ndegwa', 'fndegwa@gmail.com', '$2y$10$lSdibY.qL1w/sQuYfC1aAeCVNplmnlPLrq3VBBkuaedBJw9hLY04S', 2, '2024-02-22', 'normal'),
 (35, 'Shakira', 'Syevuo', 'shakirasyevuo@gmail.com', '$2y$10$cGwMdvZrgVJtrPEnARmujOU74y5h8tLyKyM41eiF8dn2.mbQ1FEw6', 2, '2024-04-10', 'seller'),
 (36, 'Brian', 'Kamau', 'bkk@gmail.com', '$2y$10$vZVdrw.rIXaZdatTBInne.ed0edMw.HlRiAMaxKZuYziN9HZBfPLi', 2, '2024-05-01', 'normal'),
-(37, 'Rosemary', 'Ihomba', 'rihomba72@yahoo.com', '$2y$10$RM91ZoosMEc6342ae6b7VuBCs3JR2yEUN4zz5d6BXDlYc9DyZV.8C', 2, '2024-05-01', 'seller');
+(37, 'Rosemary', 'Ihomba', 'rihomba72@yahoo.com', '$2y$10$RM91ZoosMEc6342ae6b7VuBCs3JR2yEUN4zz5d6BXDlYc9DyZV.8C', 2, '2024-05-01', 'seller'),
+(38, 'Anita', 'Gichuki', 'anitawangui04@gmail.com', '$2y$10$4QkpKEbV3ARpUQrX3Rkb5OngPxSXOl/2hXctP/IEM8dHCcSTQ0tSK', 2, '2024-05-09', 'normal'),
+(40, 'Anita', 'Gichuki', 'anitawangui77@gmail.com', '$2y$10$zbXocLQwYwbigxxHcJnwY.9u7kblHTSoCuu5a6zZVoUFOzkGK4sty', 2, '2024-05-09', 'normal');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`CategoryID`);
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `listings`
 --
 ALTER TABLE `listings`
   ADD PRIMARY KEY (`listing_id`),
-  ADD KEY `fk_seller_id` (`seller_id`);
+  ADD KEY `fk_seller_id` (`seller_id`),
+  ADD KEY `fk_category` (`category_id`),
+  ADD KEY `fk_brand` (`brand_id`);
 
 --
 -- Indexes for table `subscribers`
@@ -198,22 +265,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `CategoryID` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `listings`
 --
 ALTER TABLE `listings`
-  MODIFY `listing_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `listing_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- Constraints for dumped tables
@@ -223,6 +296,8 @@ ALTER TABLE `users`
 -- Constraints for table `listings`
 --
 ALTER TABLE `listings`
+  ADD CONSTRAINT `fk_brand` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`brand_id`),
+  ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
   ADD CONSTRAINT `fk_seller_id` FOREIGN KEY (`seller_id`) REFERENCES `users` (`UserId`);
 
 --
